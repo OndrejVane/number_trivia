67b8ca48b14f7bd44515c53168bdb232cd084e79
Ondřej Váně <onry.vane@gmail.com>
2023-03-08 20:29:04 +0100

    Implemented all domain layer

---
+++ lib/features/number_trivia/domain/use_cases/get_random_number_trivia.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:number_trivia/core/error/failure.dart';
+
+import '../../../../core/use_cases/use_case.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams noParams) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
--- test/features/number_trivia/domain/use_cases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/use_cases/get_random_number_trivia_test.dart
@@ -1,10 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/use_cases/use_case.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:number_trivia/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart';
 import 'package:mockito/annotations.dart';
+import 'package:number_trivia/features/number_trivia/domain/use_cases/get_random_number_trivia.dart';
 
 import 'get_concrete_number_trivia_test.mocks.dart';
 
@@ -13,31 +14,30 @@ class MockNumberTriviaRepository extends Mock
 
 @GenerateMocks([MockNumberTriviaRepository])
 void main() {
-  late final GetConcreteNumberTrivia usecase;
+  late final GetRandomNumberTrivia usecase;
   late final MockMockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockMockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
-  const tNumber = 1;
   const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
 
   test(
-    'should get trivia for the number from the repository',
-        () async {
+    'should get random trivia from the repository',
+    () async {
       // arrange
-      when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+      when(mockNumberTriviaRepository.getRandomNumberTrivia())
           .thenAnswer((_) async => const Right(tNumberTrivia));
 
       // act
-      final result = await usecase(const Params(number: tNumber));
+      final result = await usecase(NoParams());
 
       // assert
       expect(result, const Right(tNumberTrivia));
       // Verify that the method has been called on the Repository
-      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verify(mockNumberTriviaRepository.getRandomNumberTrivia());
       // Only the above method should be called and nothing more.
       verifyNoMoreInteractions(mockNumberTriviaRepository);
     },
